14bbff91e3f9e84e4e05ca8242e2a2e3bc9b5512
Barlet <yaroslavbarlet@gmail.com>
2017-11-09 16:12:51 +0200

    End of the day 26

---
+++ Day 26/_partials/footer.php
@@ -0,0 +1,8 @@
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
+<script src="http://cloud.github.com/downloads/wycats/handlebars.js/handlebars-1.0.0.beta.6.js"></script>
+<script src="js/scripts.js"></script>
+
+</body>
+</html>
